7c2ad448bf4d2b56137dd1e4eebb7296f1436e7d
bcdv2006 <33175291+bcdv2006@users.noreply.github.com>
2018-01-04 10:15:18 +0700

    file main of UART
    UART project's main file

--- main.c
+++ main.c
@@ -1,41 +1,84 @@
+/*
+    PD5(U1TX)
+    PD6(U1RX)
+*/
 #include "stm8s.h"
-#include "stm8s_tim1.h"
-#include "stm8s_adc1.h"
-#include "stm8s_ds1307.h"
-uint8_t data = 0;
-u8* sec;
-
-void Delay (uint16_t nCount);
-
+//#include "stdio.h"
+//#define PUTCHAR_PROTOTYPE int putchar (int c)
+//#define GETCHAR_PROTOTYPE int getchar (void)
+int l=0;
+void puts(char*s);
 void main(void)
 {
-    CLK_HSI_16MHz_Init();
-    I2C_DS1307_init();
-   
+    
+    /*High speed internal clock prescaler: 1*/
+    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
+
+    UART1_DeInit();
+    /* UART1 configuration ------------------------------------------------------*/
+    /* UART1 configured as follow:
+    - BaudRate = 115200 baud  
+    - Word Length = 8 Bits
+    - One Stop Bit
+    - No parity 
+    - Receive and transmit enabled
+    - UART1 Clock disabled
+    */
+    UART1_Init((uint32_t)9600, UART1_WORDLENGTH_8D, UART1_STOPBITS_1, UART1_PARITY_NO,
+    UART1_SYNCMODE_CLOCK_DISABLE, UART1_MODE_TXRX_ENABLE);
+
+    /* Output a message on Hyperterminal using printf function */
+    //printf("\n\rUART1 Example :retarget the C library printf()/getchar() functions to the UART\n\r");
+    //printf("\n\rEnter Text\n\r");
+    
+//    char a[]="Mai Trong Nhan\nKim Nga Lee";
+      puts("Mai Trong Nhan");
+//    int i=0;
+//    for(i=0;i<sizeof(a)-1;i++)
+//    {
+//     UART1_SendData8(a[i]);
+//      while (UART1_GetFlagStatus(UART1_FLAG_TXE) == RESET);
+//    }
     while (1)
     {
-      I2C_DS1307_ByteRead(0x00, sec);
-      Delay(10000);
+     //   ans = getchar();
+    //    printf("%c", ans);  
     }
 }
 
-/**
-  * @brief Delay
-  * @param nCount
-  * @retval None
-  */
-void Delay(uint16_t nCount)
-{
-    /* Decrement nCount value */
-    while (nCount != 0)
+
+void puts(char*s)
+{ 
+  int i=0;
+  l = sizeof(s)-1;
+     for(i=0;i<l;i++)
     {
-        nCount--;
+      UART1_SendData8(s[i]);
+      while (UART1_GetFlagStatus(UART1_FLAG_TXE) == RESET);
     }
 }
+/**
+PUTCHAR_PROTOTYPE
+{
+    UART1_SendData8(c);
+    while (UART1_GetFlagStatus(UART1_FLAG_TXE) == RESET);
 
+    return (c);
+}
 
-
-#ifdef USE_FULL_ASSERT          
+GETCHAR_PROTOTYPE
+{
+#ifdef _COSMIC_
+  char c = 0;
+#else
+  int c = 0;
+#endif
+  while (UART1_GetFlagStatus(UART1_FLAG_RXNE) == RESET);
+    c = UART1_ReceiveData8();
+  return (c);
+}
+**/
+#ifdef USE_FULL_ASSERT
 
 /**
   * @brief  Reports the name of the source file and the source line number
